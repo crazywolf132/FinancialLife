360cfadea5bf65aebb32ef00e72c6b263b1189f5
crazywolf <crazywolf132@gmail.com>
2019-02-12 19:27:23 +1100

    Updated expense.cpp

--- expense.cpp
+++ expense.cpp
@@ -4,6 +4,10 @@ Expense::Expense(double amt, Account tFrom) {
     Expense("", amt, tFrom, false);
 }
 
+Expense::Expense(double amt, Account tFrom, bool oT) {
+    Expense("", amt, tFrom, oT);
+}
+
 Expense::Expense(string n, double amt, Account tFrom, bool oT) {
     Expense(n, amt, tFrom, oT, "monthly");
 }
