20fe6f1216bfb1bab0b62c9ebcded622b4255545
crazywolf <crazywolf132@gmail.com>
2019-02-12 19:11:01 +1100

    Filled expense.h

---
+++ expense.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "account.h"
+
+using namespace std;
+
+class Expense {
+    public:
+        Expense( ) = default;
+        Expense(double amt, Account takeFrom);
+        Expense(string name, double amount, Account takeFrom, bool oneTime);
+        Expense(string name, double amount, Account takeFrom, bool oneTime, string recurringType);
+
+    private:
+        string name = "";
+        double amount = 0.00;
+        bool oneTime = false;
+        string recurringType = "monthly";
+        Account takeFrom = Account( );
+
+};
